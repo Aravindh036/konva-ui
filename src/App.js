00ffa61591d66ee1cc165fcb516b1171cb003360
Aravindh036 <aravindh036@gmail.com>
2019-08-11 18:19:38 +0530

    added drag and drop, updated json download available

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import json from './design.json';
-import { parseJson } from './utils/index.js';
+import data from './design.json';
+import { parseJson, getJson } from './utils/index.js';
 import './App.css';
+import { Stage } from 'react-konva';
 
+var json = data;
 
 class App extends Component {
   state = {
+    stageScale: 1,
+    stageX: 0,
+    stageY: 0,
     canvas: 0,
     length: 0,
-    full_canvas:[]
+    full_canvas: [{ layer: null, data: { width: null, height: null } }]
   }
   componentDidMount() {
-    const [canvas,length] = parseJson(json);
-    console.log(length);
+    const [canvas, length] = parseJson(json);
+    // console.log(length);
     this.setState({
       full_canvas: canvas,
-      length:length
+      length: length
     });
+
   }
   left = () => {
     var canvas = this.state.canvas;
@@ -34,15 +40,63 @@ class App extends Component {
       });
     }
   }
+
+  handleWheel = e => {
+    e.evt.preventDefault();
+    // console.log(this.state.full_canvas)
+    const scaleBy = 1.05;
+    const stage = e.target.getStage();
+    const oldScale = stage.scaleX();
+    const mousePointTo = {
+      x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
+      y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
+    };
+
+    const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+    stage.scale({ x: newScale, y: newScale });
+    this.setState({
+      stageScale: newScale,
+      stageX: -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale,
+      stageY: -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale
+    }, () => {
+      // console.log(this.state.full_canvas)
+    });
+    // console.log(json);
+  }
+  downloadJson = () => {
+    var element = document.createElement('a');
+    var json = getJson() ;
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(json)));
+    element.setAttribute('download', "template.yaml");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
   render() {
-    console.log("rendering");
+    // console.log("rendering", this.state.full_canvas);
     return (
       <div className="App">
         <div className="icons left-icon" onClick={this.left} >&#60;</div>
         <div className="canvas-display">
-          {this.state.full_canvas[this.state.canvas]}
+          <Stage
+            x={this.state.stageX}
+            y={this.state.stageY}
+            key={this.state.full_canvas}
+            width={this.state.full_canvas[this.state.canvas].data.width}
+            height={this.state.full_canvas[this.state.canvas].data.height}
+            onWheel={this.handleWheel} >
+            {this.state.full_canvas[this.state.canvas].layer}
+          </Stage>
         </div>
         <div className="icons right-icon" onClick={this.right} >&#62;</div>
+        <div>
+          <button className="download" onClick={this.downloadJson} >Download</button>
+        </div>
       </div>
     )
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,132 @@
 import React from 'react';
 import { getColor, getPath } from './helper';
 import useImage from 'use-image';
-// import { View, Group, Rectangle, Circle, Line, Path, PointText, Raster } from 'react-paper-bindings';
-import { Layer, Stage, Rect, Circle, Line, Path, Text, Image, Group } from 'react-konva';
-function parseJson(json) {
-    var result=[], j = 0;
-    for (var i of json) {
-        j++;
-        result.push(label[i.tag](i));
-    }
-    console.log(result,j);
-    return [result,j];
+import { Layer, Rect, Circle, Line, Path, Text, Image, Group } from 'react-konva';
+var alteredJSON = null;
+
+function getJson() {
+    return alteredJSON;
 }
 
-const myfun = (e)=>{
-    console.log(e);
+function updatePositionJson(id, x, y) {
+    console.log("id", id);
+    console.log("x", x);
+    console.log("y", y);
+    console.log(alteredJSON);
+    var j = 0;
+    for (var i of alteredJSON) {
+        alteredJSON[j] = findComponent[i.tag](i, id, x, y);
+        j++;
+    }
+    console.log(alteredJSON);
+}
+
+const findComponent = {
+    'artboard': function (json, id, x, y) {
+        var l = 0;
+        if (json.id === id) {
+            console.log(json.tag, id, x, y);
+        }
+        else {
+            for (var layers of json.layers) {
+                json.layers[l] = findComponent[layers.tag](layers, id, x, y);
+                l++;
+            }
+        }
+        return json;
+    },
+    'group': function (json, id, x, y) {
+        var k = 0;
+        if (json.id === "afd946e2-7528-4bdf-a574-edd50775f1f5") {
+            console.log(json);
+        }
+        if (json.id === id) {
+            console.log(json.tag, id, x, y);
+        }
+        else {
+            for (var layers of json.layers) {
+                // console.log(layers)
+                json.layers[k] = findComponent[layers.tag](layers, id, x, y);
+                k++;
+            }
+        }
+        return json;
+    },
+    'rectangle': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'circle': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'path': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'text': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'image': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'line': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    },
+    'svg': function (json, id, x, y) {
+        if (json.id === id) {
+            json.frame.x = x;
+            json.frame.y = y;
+            console.log(json.tag, id, x, y);
+        }
+        return json;
+    }
+}
+
+function parseJson(json) {
+    var result = [], j = 0;
+    for (var i of json) {
+        j++;
+        result.push({
+            layer: label[i.tag](i),
+            data: {
+                height: i.frame.height,
+                width: i.frame.width
+            }
+        });
+    }
+    // console.log(result, j);
+    if (alteredJSON == null) {
+        alteredJSON = json;
+    }
+    return [result, j];
 }
 
 const label = {
@@ -23,12 +135,10 @@ const label = {
         for (var layers of json.layers) {
             layersArray.push(label[layers.tag](layers));
         }
-
-        return <Stage key={json.frame.id} height={json.frame.height} width={json.frame.width} scal >
-            <Layer key={json.frame.id} >
-                {layersArray}
-            </Layer>
-        </Stage>;
+        return <Layer
+            key={json.frame.id}>
+            {layersArray}
+        </Layer>;
     },
     'group': function (json) {
         var layersArray = [];
@@ -44,6 +154,7 @@ const label = {
     },
     'rectangle': function (json) {
         var obj = {
+            ComponentKey: json.id,
             key: json.id,
             x: parseInt(json.frame.x.toFixed(2)),
             y: parseInt(json.frame.y.toFixed(2)),
@@ -68,12 +179,16 @@ const label = {
             obj['strokeWidth'] = json.style.border[index].thickness;
             obj['stroke'] = getColor(json.style.border[index].color.value);
         }
-        console.log("json::", json.name, json);
-        return <Rect {...obj} draggable/>;
+        // console.log("json::", json.name, json);
+        return <Rect {...obj} draggable onDragEnd={(e) => {
+            json.frame.hello = "praveen"
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y)
+        }} />;
     },
     'circle': function (json) {
         var obj = {
             key: json.id,
+            ComponentKey: json.id,
             x: parseInt(json.frame.x.toFixed(2)) + json.radius,
             y: parseInt(json.frame.y.toFixed(2)) + json.radius - 5,
             radius: json.radius
@@ -95,11 +210,15 @@ const label = {
             obj['stroke'] = getColor(json.style.border[index].color.value);
         }
         // console.log("in circle",obj);
-        return <Circle {...obj} />;
+        return <Circle {...obj} draggable onDragEnd={(e) => {
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y);
+            console.log(json);
+        }} />;
     },
     'path': function (json) {
         var obj = {
             key: json.id,
+            ComponentKey: json.id,
             x: parseInt(json.frame.x.toFixed(2)),
             y: parseInt(json.frame.y.toFixed(2)),
             height: parseInt(json.frame.height.toFixed(2)),
@@ -110,35 +229,41 @@ const label = {
             obj['fill'] = getColor(json.style.backgroundColor[index].value);
         }
         // console.log(json);
-        return <Path {...obj} />;
+        return <Path {...obj} draggable onDragEnd={(e) => {
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y);
+            console.log(json);
+        }} />;
     },
     'text': function (json) {
         var obj = {
+            ComponentKey: json.id,
             key: json.id,
             x: parseInt(json.frame.x.toFixed(2)),
             y: parseInt(json.frame.y.toFixed(2)),
             height: json.frame.height,
             width: json.frame.width
         }
-        console.log("text", json);
+        // console.log("text", json);
         for (let index in json.style.format) {
             // console.log(json.style.format[index]);
             obj['fontFamily'] = json.style.format[index].fontFamily;
-            if(json.style.format[index].fontStyle === "ExtraBold")
-            obj['fontStyle'] = 1000;
+            if (json.style.format[index].fontStyle === "ExtraBold")
+                obj['fontStyle'] = 1000;
             else if ((json.style.format[index].fontStyle !== "Light") && ((json.style.format[index].fontStyle !== "Medium")))
                 obj['fontStyle'] = json.style.format[index].fontStyle;
             obj['fontSize'] = json.style.format[index].fontSize;
             obj['fontSize'] = json.style.format[index].fontSize;
-
         }
         obj['fill'] = getColor(json.color[0].value);
         obj['text'] = json.string;
 
-        return <Text {...obj} />;
+        return <Text {...obj} draggable onDragEnd={(e) => {
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y);
+            console.log(json);
+        }} />;
     },
     'image': function (json) {
-        return <CustomImage json={json} />;
+        return <CustomImage key={json.id} json={json} />;
     },
     'line': function (json) {
         var x, y;
@@ -155,6 +280,7 @@ const label = {
             y = parseInt(json.frame.y.toFixed(2));
         }
         var obj = {
+            ComponentKey: json.id,
             key: json.id,
             x: x,
             y: y,
@@ -166,19 +292,26 @@ const label = {
             obj['stroke'] = getColor(json.style.stroke[index].color.value);
         }
         // console.log("in line ", obj);
-        return <Line {...obj} />;
+        return <Line {...obj} draggable onDragEnd={(e) => {
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y);
+            console.log(json);
+        }} />;
     },
     'svg': function (json) {
-        console.log(json);
-        return <Image />
+        return <Image key={json.id} draggable onDragEnd={(e) => {
+            updatePositionJson(json.id, e.target.attrs.x, e.target.attrs.y);
+            console.log(json);
+        }} />
     }
 }
 
 
 const CustomImage = (props) => {
     var [imageObj] = useImage(`https://konva.s3.ap-south-1.amazonaws.com/resources/${props.json.src}.png`);
-    console.log(props)
+    // console.log(props)
     var obj = {
+        ComponentKey: props.json.id,
+        key: props.json.id,
         x: props.json.frame.x,
         y: props.json.frame.y,
         height: props.json.frame.height,
@@ -188,8 +321,11 @@ const CustomImage = (props) => {
     for (let index in props.json.style.border) {
         obj['stroke'] = getColor(props.json.style.border[index].color.value);
     }
-    console.log(obj);
-    return (<Image {...obj} />);
+    // console.log(obj);
+    return (<Image {...obj} draggable onDragEnd={(e) => {
+        updatePositionJson(props.json.id, e.target.attrs.x, e.target.attrs.y);
+        console.log(props.json);
+    }} />);
 }
 
-export { parseJson };
+export { parseJson, getJson };
